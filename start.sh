eed23b10174b33df36d8dfd997ceaec3de7255a0
Ankur Maharjan <akrmhrjn@MacMini.local>
2016-02-29 17:42:09 +0545

    updated script file

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-nc -k -l 5550 > /home/logs/backend.out &
-nc -k -l 5551 > /home/logs/lint.out &
-nc -k -l 5552 > /home/logs/yslow.out &
-nc -k -l 5553 > /home/logs/defce.out &
-nc -k -l 5554 > /home/logs/head.out &
-nc -k -l 5555 > /home/logs/full.out &
-nc -k -l 5556 > /home/logs/narad.out &
-nc -k -l 5557 > /home/logs/scheduler.out 
+nc -k -l 5550 >> /home/logs/backend.out &
+nc -k -l 5551 >> /home/logs/lint.out &
+nc -k -l 5552 >> /home/logs/yslow.out &
+nc -k -l 5553 >> /home/logs/deface.out &
+nc -k -l 5554 >> /home/logs/head.out &
+nc -k -l 5555 >> /home/logs/full.out &
+nc -k -l 5556 >> /home/logs/narad.out &
+nc -k -l 5557 >> /home/logs/scheduler.out 
